dfe99669704755048fdff9566a6f4c4e5c105b19
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2019-11-22 21:50:32 -0600

    Done

--- Assets/Example03 - Runtime/LevelGenerator.cs
+++ Assets/Example03 - Runtime/LevelGenerator.cs
@@ -1,43 +1,53 @@
 using UnityEngine;
+using UnityEngine.AI;
 
-public class LevelGenerator : MonoBehaviour {
+/*
+ Entirely Brackey's code https://learn.unity.com/tutorial/unity-navmesh#5c7f8528edbc2a002053b498
+ No changes made
+     */
 
-	public int width = 10;
-	public int height = 10;
+public class LevelGenerator : MonoBehaviour
+{
 
-	public GameObject wall;
-	public GameObject player;
+    public NavMeshSurface surface;
+    public int width = 10;
+    public int height = 10;
 
-	private bool playerSpawned = false;
+    public GameObject wall;
+    public GameObject player;
 
-	// Use this for initialization
-	void Start () {
-		GenerateLevel();
-	}
-	
-	// Create a grid based level
-	void GenerateLevel()
-	{
-		// Loop over the grid
-		for (int x = 0; x <= width; x+=2)
-		{
-			for (int y = 0; y <= height; y+=2)
-			{
-				// Should we place a wall?
-				if (Random.value > .7f)
-				{
-					// Spawn a wall
-					Vector3 pos = new Vector3(x - width / 2f, 1f, y - height / 2f);
-					Instantiate(wall, pos, Quaternion.identity, transform);
-				} else if (!playerSpawned) // Should we spawn a player?
-				{
-					// Spawn the player
-					Vector3 pos = new Vector3(x - width / 2f, 1.25f, y - height / 2f);
-					Instantiate(player, pos, Quaternion.identity);
-					playerSpawned = true;
-				}
-			}
-		}
-	}
+    private bool playerSpawned = false;
 
+    // Use this for initialization
+    void Start()
+    {
+        GenerateLevel();
+        surface.BuildNavMesh();
+    }
+
+    // Create a grid based level
+    void GenerateLevel()
+    {
+        // Loop over the grid
+        for (int x = 0; x <= width; x += 2)
+        {
+            for (int y = 0; y <= height; y += 2)
+            {
+                // Should we place a wall?
+                if (Random.value > .7f)
+                {
+                    // Spawn a wall
+                    Vector3 pos = new Vector3(x - width / 2f, 1f, y - height / 2f);
+                    Instantiate(wall, pos, Quaternion.identity, transform);
+                }
+                else if (!playerSpawned) // Should we spawn a player?
+                {
+                    // Spawn the player
+                    Vector3 pos = new Vector3(x - width / 2f, 1.25f, y - height / 2f);
+                    Instantiate(player, pos, Quaternion.identity);
+                    playerSpawned = true;
+                }
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/PlayerControllerNew.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+/*
+ Still Brackey's code, just made a separate version to not conflict with previous scenes. No changes have been made.
+     
+     */
+
+public class PlayerControllerNew : MonoBehaviour
+{
+
+    public Camera cam;
+
+    public NavMeshAgent agent;
+
+    public ThirdPersonCharacter character;
+
+    void Start()
+    {
+        agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            character.Move(Vector3.zero, false, false);
+        }
+
+    }
+}
